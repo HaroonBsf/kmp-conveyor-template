21fca020119190c9d0a85b8271b544e6dbe9915a
Haroon Majeed <haroonbsf786@gmail.com>
2025-09-12 04:17:48 +0500

    configure localhost site for testing

--- composeApp/src/jvmMain/kotlin/com/haroon/kmpconveyor/main.kt
+++ composeApp/src/jvmMain/kotlin/com/haroon/kmpconveyor/main.kt
@@ -1,13 +1,30 @@
 package com.haroon.kmpconveyor
 
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.nio.file.Paths
+import kotlin.io.path.exists
+import kotlin.io.path.inputStream
 
 fun main() = application {
+    val appIcon = rememberAppIcon()
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "kmp-conveyor-template",
+        icon = appIcon,
+        title = "KMP Conveyor Template"
     ) {
         App()
     }
+}
+
+fun rememberAppIcon(): BitmapPainter? {
+    return System.getProperty("app.dir")
+        ?.let { Paths.get(it, "icon-512.png") }
+        ?.takeIf { it.exists() }
+        ?.inputStream()
+        ?.buffered()
+        ?.use { BitmapPainter(loadImageBitmap(it)) }
 }
